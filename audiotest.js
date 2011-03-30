8d05764c43715e2e739f847df3c94322ab11f113
Joe Turner <joe@oampo.co.uk>
2011-03-30 17:31:02 +0100

    Add simple function for running tests

--- audiotest.js
+++ audiotest.js
@@ -9,28 +9,28 @@ var Assert = {
     assertTrue: function(actual, msg) {
         if (actual !== true) {
             throw (msg || 'assertEquals failed') +
-                '. Expected true';
+                '. Expected true' + '; Actual: ' + actual;
         }
     },
 
     assertFalse: function(actual, msg) {
         if (actual !== false) {
             throw (msg || 'assertEquals failed') +
-                '. Expected false';
+                '. Expected false' + '; Actual: ' + actual;
         }
     },
 
     assertNull: function(actual, msg) {
         if (actual !== null) {
             throw (msg || 'assertNull failed') +
-                '. Expected null';
+                '. Expected null' + '; Actual: ' + actual;
         }
     },
 
     assertNotNull: function(actual, msg) {
         if (actual === null) {
             throw (msg || 'assertNotNull failed') +
-                '. Expected not null';
+                '. Expected not null' + '; Actual: ' + actual;
         }
     },
 
@@ -38,3 +38,31 @@ var Assert = {
         throw (msg || 'fail');
     }
 };
+
+function test(name, test, iterations) {
+    iterations = iterations || 1;
+    var startTime = new Date().getTime();
+    var passed = true;
+    var exception = null;
+    try {
+        for (var i=0; i<iterations; i++) {
+            test();
+        }
+    }
+    catch (e) {
+        passed = false;
+        exception = e;
+    }
+    var endTime = new Date().getTime();
+    var totalTime = endTime - startTime;
+    var averageTime = totalTime / iterations;
+    if (passed) {
+        print('Test ' + name + ' passed');
+        print(iterations + ' iterations in ' + totalTime + ' ms, ' +
+              averageTime + ' ms per iteration');
+    }
+    else {
+        print('Test ' + name + ' FAILED');
+        print(exception);
+    }
+}
